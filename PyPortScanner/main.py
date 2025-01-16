dbc9eed4d591f36cb2105b5000202d2f09759568
etaylor224 <etay279@wgu.edu>
2025-01-15 21:15:39 -0600

    Update main.py

--- PyPortScanner/main.py
+++ PyPortScanner/main.py
@@ -6,10 +6,47 @@ class Port_Scanner():
         self.targets = targets
         self.start_port = 1
         self.end_port = 65536
-                
+
+        self.ip_convert()
+
+    def ip_convert(self):
+
+        target_lst = list()
+
+        if type(self.targets) == list:
+            for target in targets:
+                try:
+                    ipaddress.ip_address(target)
+                except ValueError:
+                    print(f"{self.targets} is an invalid IP Address")
+                else:
+                    target_lst.append(target)
+            self.targets = target_lst
+
+        elif "/" in self.targets:
+            for addr in ipaddress.IPv4Network(self.targets):
+                target_lst.append(addr)
+            self.targets = target_lst
+
+        elif "," in self.targets:
+            ip = self.targets.split(",")
+            for target in ip:
+                try:
+                    ipaddress.ip_address(target)
+                except ValueError:
+                    print(f"{self.targets} is an invalid IP Address")
+                else:
+                    target_lst.append(target)
+            self.targets = target_lst
+        else:
+            try:
+                ipaddress.ip_address(self.targets)
+            except ValueError:
+                print(f"{self.targets} is an invalid IP Address")
 
     def scan_port(self, port, ip):
         try:
+            print(f"Scanning host {ip} on port {port}")
             sock = socket.socket()
             sock.settimeout(0.5)
             sock.connect((ip, port))
@@ -20,19 +57,33 @@ class Port_Scanner():
                 print(f"Open Port {port}")
 
         except:
-            print(f"Port {port} Closed")
-
-    def multiple_ports(self, *args):
-        ip = self.targets
-
-        if args:
             pass
 
-        for port in range(self.start_port, (self.end_port + 1)):
-            self.scan_port(port, ip)
+    def multiple_ports(self, *args):
+
+        if args:
+            ports = args[0]
+            self.start_port = int(ports[0])
+            self.end_port = int(ports[1])
+
+        if type(self.targets) == list:
+
+            for target in self.targets:
+
+                for port in range(self.start_port, (self.end_port + 1)):
+                    self.scan_port(port, target)
+        else:
+            for port in range(self.start_port, self.end_port + 1):
+                self.scan_port(port, self.targets)
 
     def single_port(self, port):
-        think = 0
+
+        if type(self.targets) == list:
+
+            for target in self.targets:
+                self.scan_port(port, target)
+        else:
+            self.scan_port(port, self.targets)
 
 
 targets = input('Enter Target/s To Scan (split multiple targets with "," Can also enter CIDR notation): ')
@@ -42,8 +93,8 @@ scanner = Port_Scanner(targets)
 
 if port_type == "1":
     port_range_to_scan = input("Please enter the port range ex 1-22: ")
-    range = port_range_to_scan.split("-")
-    #scanner.multiple_ports(range) BROKEN
+    ranges = port_range_to_scan.split("-")
+    scanner.multiple_ports(ranges)
 
 elif port_type == "2":
     port_to_scan = int(input("Enter the port to be scanned: "))
