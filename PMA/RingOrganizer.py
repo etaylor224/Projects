05d7c6f823e318233bca5d1b0843bdb6af7f7d77
etaylor224 <etay279@wgu.edu>
2025-05-07 15:40:01 -0500

    Update RingOrganizer.py

--- PMA/RingOrganizer.py
+++ PMA/RingOrganizer.py
@@ -1,16 +1,27 @@
+import datetime
+
 import pandas as pd
 import tabula
-#Note this is tabula-py lib
+from tkinter import filedialog
 
+
+def read_pdf_to_df(multi_table : bool = False):
+
+    pdf_path = filedialog.askopenfilename()
+    pdf_read = tabula.read_pdf(pdf_path, pages="all", multiple_tables=multi_table)
+    return pd.DataFrame(pdf_read[0])
 
 def imas_pdf_to_excel():
-    pdf_path = r"C:\Users\bassj\Downloads\nola_rings.pdf"
-
-    dfs = tabula.read_pdf(pdf_path, pages='all', multiple_tables=False)
-    ring_df = pd.DataFrame(dfs[0])
+    
+    ring_df = read_pdf_to_df()
     sorted_by_rdt = ring_df.sort_values(by=["Ring", "Day", "Time"])
     sorted_by_rdt.to_excel('sorted_rings.xlsx', index=False)
     return
 
+def first_pdf_to_excel():
+    df = read_pdf_to_df()
+    sort_df = df.sort_values(by=["Division", "Ring", "Time"])
+    today = datetime.date.today()
+    sort_df.to_excel(f"sorted_rings_{today}.xlsx", index=False)
 
-imas_pdf_to_excel()
+first_pdf_to_excel()
